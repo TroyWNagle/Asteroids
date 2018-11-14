101df2ec195c7854361f10c894132e08aad93001
TroyWNagle <troywnagle@gmail.com>
2018-11-14 15:59:54 -0800

    Updates

--- precache-manifest.983b5560cf931a292ab3553cc99ad964.js
+++ precache-manifest.983b5560cf931a292ab3553cc99ad964.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/Asteroids/static/js/runtime~main.e8d3366d.js"
   },
   {
-    "revision": "08e27e375f1a3ea57bd9",
-    "url": "/Asteroids/static/js/main.08e27e37.chunk.js"
+    "revision": "51d46fc10b329c1b950a",
+    "url": "/Asteroids/static/js/main.51d46fc1.chunk.js"
   },
   {
-    "revision": "08e27e375f1a3ea57bd9",
+    "revision": "51d46fc10b329c1b950a",
     "url": "/Asteroids/static/css/main.498ffa67.chunk.css"
   },
   {
-    "revision": "70dd6147611bb9f4f48aaf3b6ece6f87",
+    "revision": "56d9bc4a33f76e5035c9537ee7fd379f",
     "url": "/Asteroids/index.html"
   }
 ];
