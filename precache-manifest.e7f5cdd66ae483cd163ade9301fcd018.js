077c8e10a054581e6d1f25b17d917d46358478f9
TroyWNagle <troywnagle@gmail.com>
2018-11-19 21:27:34 -0800

    Updates

---
+++ precache-manifest.e7f5cdd66ae483cd163ade9301fcd018.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "e8d3366d8fd5a9a58065",
+    "url": "/Asteroids/static/js/runtime~main.e8d3366d.js"
+  },
+  {
+    "revision": "7e8dbfd53f13217e349e",
+    "url": "/Asteroids/static/js/main.7e8dbfd5.chunk.js"
+  },
+  {
+    "revision": "7e8dbfd53f13217e349e",
+    "url": "/Asteroids/static/css/main.498ffa67.chunk.css"
+  },
+  {
+    "revision": "210e8818d52ee0de7c6f7f4a76946ad5",
+    "url": "/Asteroids/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Asteroids/precache-manifest.7f4e0e9aa318e661f7f2670d1345a723.js"
+  "/Asteroids/precache-manifest.e7f5cdd66ae483cd163ade9301fcd018.js"
 );
 
 workbox.clientsClaim();
